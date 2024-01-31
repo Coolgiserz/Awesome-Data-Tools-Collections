35849903f9629bf2a0dd9559f1cb091c6af50cee
weirdgiser <0106160226@csu.edu.cn>
2024-01-31 20:52:50 +0800

    feat: 支持多key替换爬取，当一个key额度用完使用下一个key继续爬取

--- address2coordinate/main.py
+++ address2coordinate/main.py
@@ -3,17 +3,45 @@
 # @Function:
 import random
 import time
-
+import logging
 import requests
+from collections import deque
 from settings import AMAP_KEY
 from pipelines.tasks import MailingAddress2CoordPipeline
 from geoalchemy2.functions import ST_MakePoint
+
 api = f"https://restapi.amap.com/v3/geocode/geo"
+logger = logging.getLogger("search_address")
+
 
 class KeyUsageExceedLimitException(Exception):
     pass
-def search_address(address):
-    params = {"key": AMAP_KEY,
+
+
+def key_initialization():
+    q = deque()
+    with open("keys.txt", "r") as f:
+        for line in f.readlines():
+            q.append(line.strip())
+    print(f"loading {len(q)} keys.")
+    return q
+
+
+def fetcher(api, params):
+    res = requests.get(api, params=params, proxies=None)
+    if res.status_code == 200:
+        data = res.json()
+        info = data.get("info", None)
+        if info == "USER_DAILY_QUERY_OVER_LIMIT":
+            raise KeyUsageExceedLimitException()
+        geocodes = data.get("geocodes", [])
+        return geocodes
+    else:
+        return None
+
+
+def search_address(address, key):
+    params = {"key": key,
               "address": address}
     res = requests.get(api, params=params, proxies=None)
     if res.status_code == 200:
@@ -34,75 +62,66 @@ def search_address(address):
             street = geocode.get("street", None)
             level = geocode.get("level", None)
             if formatted_address is not None:
-                results.append([address, formatted_address, location, adcode, country,province, city, district, street, level])
+                results.append(
+                    [address, formatted_address, location, adcode, country, province, city, district, street, level])
         return results
     else:
         return None
 
+
 def main():
+    key_queue = key_initialization()
     p = MailingAddress2CoordPipeline()
+    current_key = key_queue.pop()
     while True:
         objs = p.get_addresses()
         print(f"{len(objs)}")
         if len(objs) == 0:
             print("=======任务结束======")
             break
-        try:
-            for obj in objs:
-                mailing_address = obj[1]
-                print(obj[0], mailing_address )
-                result_list = search_address(mailing_address)
-                if result_list is None:
-                    p.write_addresses_point(search_keyword=mailing_address,status=False)
+        for obj in objs:
+            mailing_address = obj[1]
+            print(obj[0], mailing_address)
+            result_list = None
+            try:
+                result_list = search_address(mailing_address, key=current_key)
+            except KeyUsageExceedLimitException:
+                print("Key额度用尽, 尝试更换Key...")
+                current_key = key_queue.pop()
+                continue
 
-                for i, result in enumerate(result_list):
-                    coord = result[2].split(",")
-                    # print(result[2], coord)
-                    formatted_address = result[1]
-                    # point = ST_GeomFromText(f"POINT({result[2]})")
-                    point = ST_MakePoint(coord[0], coord[1])
-                    # point = f"POINT({result[2]})"
-                    # point =result[2].replace(","," ").replace("，"," ")
+            if result_list is None:
+                logger.info(f"search address failed! {mailing_address}")
+                # p.write_addresses_point(search_keyword=mailing_address,status=False)
 
-                    adcode = result[3]
-                    country = result[4]
-                    province = result[5]
-                    city = result[6]
-                    district = result[7]
-                    street = result[8]
-                    level = result[9]
-                    p.write_addresses_point(search_keyword=mailing_address,
-                                            formatted_address=formatted_address,
-                                            adcode=adcode,
-                                            country_name=country,
-                                            province_name=province,
-                                            city_name=city,
-                                            district=district,
-                                            street=street,
-                                            level=level,
-                                            geo=point,
-                                            status=True)
-                    if i==0:
-                        p.company_dao.write_mailing_address_point(mailing_address=mailing_address,
+            for i, result in enumerate(result_list):
+                coord = result[2].split(",")
+                formatted_address = result[1]
+                point = ST_MakePoint(coord[0], coord[1])
+                adcode = result[3]
+                country = result[4]
+                province = result[5]
+                city = result[6]
+                district = result[7]
+                street = result[8]
+                level = result[9]
+                p.write_addresses_point(search_keyword=mailing_address,
+                                        formatted_address=formatted_address,
+                                        adcode=adcode,
+                                        country_name=country,
+                                        province_name=province,
+                                        city_name=city,
+                                        district=district,
+                                        street=street,
+                                        level=level,
+                                        geo=point,
+                                        status=True)
+                if i == 0:
+                    p.company_dao.write_mailing_address_point(mailing_address=mailing_address,
                                                               point=point)
-        except KeyUsageExceedLimitException:
-            print("Key额度用尽")
-            break
-        time.sleep(random.randint(3,6))
+        time.sleep(random.randint(3, 6))
     print("Done")
+
+
 if __name__ == "__main__":
-    # api = f"https://restapi.amap.com/v3/geocode/geo?key={key}&address=广州塔"
-
-    # # datas = ["东南大学", "中南大学", "中北大学", "西南大学", "西北大学"]
-    # # TODO get data from company table
-    # datas = ["北京大学", "西南大学"]
-    # results = []
-    # for data in datas:
-    #     r = search_address(data)
-    #     results.extend(r)
-    #     # TODO store the coordinate to log table
-    #     print(f"{data}: {r}")
-    # pd.DataFrame(results, columns=["search_address", "formatted_address", "location", "adcode", "country","province", "city", "district", "street", "level"]).to_excel("test.xlsx")
-    # res = requests.get(api, proxies=None)
-
     main()
